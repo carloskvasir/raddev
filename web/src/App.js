97a5465e1cd67bcda266e800401b9eac2945d40e
Carlos Lima (Kvasir) <carloskvasir@gmail.com>
2020-01-21 18:59:59 -0300

    explicação basica sobre react

--- web/src/App.js
+++ web/src/App.js
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useState } from 'react';
 
-// Componente
-// Estado
-// Propriedade
+// Componente: Bloco isolado de HTML, CSS e JS, o qual não interfere no restante da aplicação
+// Propriedade: Informações que um componente PAI passa para o componente FILHO
+// Estado: Informações mantidas pelo componente (Lembrar: imutabilidade)
+
+import Header from './Header';
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
+  function incrementCounter() {
+    setCounter(counter + 1);
+  }
   return (
-    <h1>Hello world</h1>
+    <>
+      <Header title="header 01" />
+      <Header title="header 02" />
+      <Header title="header 04" />
+
+      <h1>Contador: {counter}</h1>
+      <button onClick={incrementCounter}>Incrementar</button>
+    </>
   );
 }
 
