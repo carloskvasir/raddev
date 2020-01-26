0d3d2dba5e266246faece4bfa1081a30c5ae5485
Carlos Lima (Kvasir) <carloskvasir@gmail.com>
2020-01-26 14:39:47 -0300

    visual completo formulário de Main

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -1,9 +1,17 @@
+import { MaterialIcons } from '@expo/vector-icons';
 import {
   getCurrentPositionAsync,
   requestPermissionsAsync,
 } from 'expo-location';
 import React, { useEffect, useState } from 'react';
-import { StyleSheet, Image, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Image,
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native';
 import MapView, { Marker, Callout } from 'react-native-maps';
 
 function Main({ navigation }) {
@@ -35,28 +43,43 @@ function Main({ navigation }) {
   }
 
   return (
-    <MapView initialRegion={currentRegion} style={styles.map}>
-      <Marker coordinate={{ latitude: -16.6123413, longitude: -49.2224156 }}>
-        <Image
-          style={styles.avatar}
-          source={{
-            uri: 'https://avatars2.githubusercontent.com/u/28787559?s=460&v=4',
-          }}
+    <>
+      <MapView initialRegion={currentRegion} style={styles.map}>
+        <Marker coordinate={{ latitude: -16.6123413, longitude: -49.2224156 }}>
+          <Image
+            style={styles.avatar}
+            source={{
+              uri:
+                'https://avatars2.githubusercontent.com/u/28787559?s=460&v=4',
+            }}
+          />
+          <Callout
+            onPress={() => {
+              // navegação
+              navigation.navigate('Profile', { github_user: 'carloskvasir' });
+            }}
+          >
+            <View style={styles.callout}>
+              <Text style={styles.devName}>Carlos Lima (Kvasir)</Text>
+              <Text style={styles.devBio}>BIO S2</Text>
+              <Text style={styles.devTechs}>ReactJs, RN, Node.js</Text>
+            </View>
+          </Callout>
+        </Marker>
+      </MapView>
+      <View style={styles.searchForm}>
+        <TextInput
+          style={styles.searchInput}
+          placeholder="Buscar por Devs por techs..."
+          placeholderTextColor="#999"
+          autoCapitalize="words" // caixa alta em cada palavra
+          autoCorrect={false}
         />
-        <Callout
-          onPress={() => {
-            // navegação
-            navigation.navigate('Profile', { github_user: 'carloskvasir' });
-          }}
-        >
-          <View style={styles.callout}>
-            <Text style={styles.devName}>Carlos Lima (Kvasir)</Text>
-            <Text style={styles.devBio}>BIO S2</Text>
-            <Text style={styles.devTechs}>ReactJs, RN, Node.js</Text>
-          </View>
-        </Callout>
-      </Marker>
-    </MapView>
+        <TouchableOpacity onPress={() => {}} style={styles.loadButton}>
+          <MaterialIcons name="my-location" size={20} color="#FFF" />
+        </TouchableOpacity>
+      </View>
+    </>
   );
 }
 const styles = StyleSheet.create({
@@ -83,6 +106,41 @@ const styles = StyleSheet.create({
   },
   devTechs: {
     marginTop: 5,
+  },
+  searchForm: {
+    position: 'absolute',
+    bottom: 20,
+    left: 20,
+    right: 20,
+    zIndex: 5,
+    flexDirection: 'row',
+  },
+  searchInput: {
+    flex: 1,
+    height: 50,
+    borderRadius: 25,
+    backgroundColor: '#FFF',
+    color: '#333',
+    paddingHorizontal: 20,
+    fontSize: 16,
+    // Ios shadows
+    shadowColor: '#000',
+    shadowOpacity: 0.2,
+    shadowOffset: {
+      width: 4,
+      height: 4,
+    },
+    // Android shadows
+    elevation: 2,
+  },
+  loadButton: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    backgroundColor: '#8E4DFF',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 15,
   },
 });
 
