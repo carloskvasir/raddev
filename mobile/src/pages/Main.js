344f2f7ebb893e4818302fbb78ef0d11efd772bb
Carlos Lima (Kvasir) <carloskvasir@gmail.com>
2020-01-26 10:00:24 -0300

    mapa já sobre o Brasil

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -1,26 +1,8 @@
 import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import MapView from 'react-native-maps';
 
 function Main() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Hello OminiStack!!!</Text>
-    </View>
-  );
+  return <MapView style={{ flex: 1 }} />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#7159C1',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    color: 'white',
-    fontWeight: 'bold',
-    fontSize: 32,
-  },
-});
 
 export default Main;
--- mobile/src/pages/Profile.js
+++ mobile/src/pages/Profile.js
@@ -1,8 +1,25 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, StyleSheet, Text } from 'react-native';
 
 function Profile() {
-  return <View />;
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello OminiStack!!!</Text>
+    </View>
+  );
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#7159C1',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 32,
+  },
+});
 
 export default Profile;
