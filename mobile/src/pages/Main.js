9ba8db769253e7dd77d15ac441bba0a1d512f676
Carlos Lima (Kvasir) <carloskvasir@gmail.com>
2020-01-26 11:27:22 -0300

    monstrando imagem no marker

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -3,7 +3,7 @@ import {
   requestPermissionsAsync,
 } from 'expo-location';
 import React, { useEffect, useState } from 'react';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Image } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
 
 function Main() {
@@ -36,13 +36,27 @@ function Main() {
 
   return (
     <MapView initialRegion={currentRegion} style={styles.map}>
-      <Marker coordinate={{ latitude: -16.6123413, longitude: -49.2224156 }} />
+      <Marker coordinate={{ latitude: -16.6123413, longitude: -49.2224156 }}>
+        <Image
+          style={styles.avatar}
+          source={{
+            uri: 'https://avatars2.githubusercontent.com/u/28787559?s=460&v=4',
+          }}
+        />
+      </Marker>
     </MapView>
   );
 }
 const styles = StyleSheet.create({
   map: {
     flex: 1,
+  },
+  avatar: {
+    width: 54,
+    height: 54,
+    borderRadius: 4,
+    borderWidth: 4,
+    borderColor: '#FFF',
   },
 });
 
